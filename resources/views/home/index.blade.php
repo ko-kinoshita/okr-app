5ae6706c02364008e23372a181d402abd1d6f9ce
ko-kinoshita <ko.kinoshita@arsaga.jp>
2020-04-17 11:13:29 +0900

    ket_resultを複数用意

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -44,6 +44,38 @@
                     <h2 class="content">Key Resul</h2>
                     <p class="content">(主な結果)</p>
                 </div>
+            </div>
+        </div>
+
+        <div class="company_objective">
+            <div class="group objective">
+                <h2 class="group content">objective</h2>
+                <p class="group content">(目標)</p>
+            </div>
+
+            <div class="key_result_gather">
+                <div class="group key_result">
+                    <h2 class="content">Key Result</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="company_objective">
+            <div class="group objective">
+                <h2 class="group content">objective</h2>
+                <p class="group content">(目標)</p>
+            </div>
+
+            <div class="key_result_gather">
+                <div class="group key_result">
+                    <h2 class="content">Key Result</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
+                <div class="group key_result">
+                    <h2 class="content">Key Resul</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
                 <div class="group key_result">
                     <h2 class="content">Key Resul</h2>
                     <p class="content">(主な結果)</p>
@@ -73,6 +105,38 @@
                     <h2 class="content">Key Resul</h2>
                     <p class="content">(主な結果)</p>
                 </div>
+            </div>
+        </div>
+        <div class="individual_objective">
+            <div class="individual objective">
+                <h2 class="content">objective</h2>
+                <p class="content">(目標)</p>
+            </div>
+
+            <div class="key_result_gather">
+                <div class="individual key_result">
+                    <h2 class="content">Key Result</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="individual_objective">
+            <div class="individual objective">
+                <h2 class="content">objective</h2>
+                <p class="content">(目標)</p>
+            </div>
+
+            <div class="key_result_gather">
+                <div class="individual key_result">
+                    <h2 class="content">Key Result</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
+                <div class="individual key_result">
+                    <h2 class="content">Key Resul</h2>
+                    <p class="content">(主な結果)</p>
+                </div>
+
             </div>
         </div>
     </section>
