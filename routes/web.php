5da051933c0a8cf26e47544f26279dd06942860f
ko-kinoshita <ko.kinoshita@arsaga.jp>
2020-04-20 10:04:20 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('Home/','HomeController@index');
+Route::get('/','HomeController@index');
 Route::get('Home/add','HomeController@add');
 Route::post('Home/add','HomeController@create');
 Route::get('Home/edit','HomeController@edit');
