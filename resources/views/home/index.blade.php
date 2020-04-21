aa137bc8720a71483b1d0757c2f357f71c3ab143
ko-kinoshita <ko.kinoshita@arsaga.jp>
2020-04-21 19:10:37 +0900

    コメントアウト消去

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,6 @@
 @if (Auth::check())
 
 <header>
-    <!-- <p>User {{ $user->name . '(' . $user->email .')'}}</p> -->
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="{{ route('logout') }}"
             onclick="event.preventDefault();
