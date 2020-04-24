dbe77b2f1db90c0a0629e155379e5ac83769ae74
ko-kinoshita <ko.kinoshita@arsaga.jp>
2020-04-24 18:05:57 +0900

    以前のviewを復元しコメントアウト

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -32,7 +32,7 @@
 @endif
 
 <div class="okr">
-    <section class="section_wrap">        
+    <section class="section_wrap">
         <div class="other_than_border_right"></div>
         <div class="top_objective company_key_result">
 
@@ -54,4 +54,72 @@
 
 
 </div>
+
+<!-- 
+<div class="okr">
+    <section class="section_wrap">
+        <h1 class="title">会社</h1>
+        
+        <div class="other_than_border_right"></div>
+        <div class="top_objective company_key_result">
+            <div class="company objective">
+                @foreach($okrs as $okr)
+                    <p class="content">{{ $okr->objective }}</p>
+                @endforeach
+            </div>
+
+            <ul class="flex">
+                @foreach($okrs as $okr)
+                <li class="company key_result top_key_result">
+                    <p class="content">{{ $okr->key_result }}</p>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </section>
+
+    <section class="section_wrap">
+        <h1 class="title">部門</h1>
+        <div class="other_than_border_right"></div>
+        <ul class="flex">
+            @foreach($okrs as $okr)
+            <li class="wrap_objective">
+                <div class="group objective">
+                    <p class="group content">{{ $okr->objective }}</p>
+                </div>
+            @endforeach
+                <ul class="flex ">
+                @foreach($okrs as $okr)
+                    <li class="group key_result">
+                        <p class="content">{{ $okr->key_result }}</p>
+                    </li>
+                @endforeach
+                </ul>
+            </li>
+        </ul>
+    </section>
+
+    <section class="section_wrap">
+        <h1 class="title">個人</h1>
+        <div class="other_than_border_right"></div>
+        <ul class="flex">
+        @foreach($okrs as $okr)
+            <li class="wrap_objective">
+                <div class="individual objective">
+                    <p class="content">{{ $okr->objective  }}</p>
+                </div>
+                <ul class="flex ">
+                @foreach($okrs as $okr)
+                    <li class="individual key_result">
+                        <p class="content">{{ $okr->key_result }}</p>
+                    </li>
+                @endforeach
+                </ul>
+            </li>
+        @endforeach
+        </ul>
+    </section>
+</div> -->
+
+
 @endsection
